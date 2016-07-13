1c072fdf7cb7c6b0df1b649410309767c21056cd
larago <bingeye@foxmail.com>
2016-07-13 21:46:07 +0800

    example of how to deal with blocking in IO task

---
+++ chapter_12/iotask.py
@@ -0,0 +1,18 @@
+class IOTask:
+    def terminate(self):
+        self._running = False
+
+    def run(self, sock):
+        #sock is a socket
+        sock.settimeout(5)  # Set timeout period
+        while self._running:
+            # Perform a blocking I/O operation w/ timeout
+            try:
+                data = sock.recv(8192)
+                break
+            except socket.timeout:
+                continue
+            # Continued processing
+
+        # Terminated
+        return
